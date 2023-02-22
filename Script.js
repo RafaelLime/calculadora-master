904d8da61174f288f45e0a6c582ca31683b8a6ad
Rafael Pereira <devrafalime@proton.me>
2023-02-22 16:11:19 -0300

    Salvando em nova ramificação tentanto consertar bug

--- Script.js
+++ Script.js
@@ -1,10 +1,12 @@
+
 function calcular(){
     let Capital_inicial = document.getElementById('capital').value
     let taxa = document.getElementById('taxa').value
     let tempo = document.getElementById('tempo').value
     let montante = Capital_inicial*((taxa/100)+1)**tempo
-    document.getElementById('result').innerHTML = "Montante final = R$"+Math.round(montante)
+    document.getElementById('result').innerHTML = `Montante final = R$${Math.round(montante)}`
 }
+
 function limpar(){
     document.getElementById('result').innerHTML=""
 }
